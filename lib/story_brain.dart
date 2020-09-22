341d6817bc5dd792c41562e8fdf7a1e29c54c920
Thomas M <twsm000@gmail.com>
2020-09-22 17:26:57 -0300

    Refactoring StoryList to StoryMapper in a new file
    - Passing the responsability to StoryMapper to locate the
    next path after a choice is called from nextStory

--- lib/story_brain.dart
+++ lib/story_brain.dart
@@ -1,85 +1,55 @@
 import 'story.dart';
+import 'story_mapper.dart';
 
 enum StoryChoice { first, second }
 
 class StoryBrain {
-  List<Story> _storyList;
-  int _storyNumber;
-  Story _story;
+  StoryMapper _stories = StoryMapper();
+  Story _currentStory;
 
   StoryBrain() {
-    _storyList = StoryList.getStories();
-    _storyNumber = 0;
-    _story = _storyList.first;
-  }
-
-  bool hasNext() {
-    _storyNumber++;
-    if (_storyNumber < _storyList.length) {
-      _story = _storyList.elementAt(_storyNumber);
-      return true;
-    }
-
-    _story = null;
-    return false;
+    _currentStory = _stories.beginning;
   }
 
   String getStory() {
-    return _story.storyTitle;
+    return _currentStory.storyTitle;
   }
 
   String getFirstChoice() {
-    return _story.choice1;
+    return _currentStory.choice1;
   }
 
   String getSecondChoice() {
-    return _story.choice2;
+    return _currentStory.choice2;
   }
 
-  void nextStory(StoryChoice choice) {}
-}
+  void nextStory(StoryChoice choice) {
+    StorySplitPath path = _stories.path(_currentStory);
 
-class StoryList {
-  static List<Story> getStories() {
-    return [
-      Story(
-          storyTitle:
-              'Your car has blown a tire on a winding road in the middle of nowhere with no cell phone reception. You decide to hitchhike. A rusty pickup truck rumbles to a stop next to you. A man with a wide brimmed hat with soulless eyes opens the passenger door for you and asks: "Need a ride, boy?".',
-          choice1: 'I\'ll hop in. Thanks for the help!',
-          choice2: 'Better ask him if he\'s a murderer first.'),
-      Story(
-          storyTitle: 'He nods slowly, unphased by the question.',
-          choice1: 'At least he\'s honest. I\'ll climb in.',
-          choice2: 'Wait, I know how to change a tire.'),
-      Story(
-          storyTitle:
-              'As you begin to drive, the stranger starts talking about his relationship with his mother. He gets angrier and angrier by the minute. He asks you to open the glovebox. Inside you find a bloody knife, two severed fingers, and a cassette tape of Elton John. He reaches for the glove box.',
-          choice1: 'I love Elton John! Hand him the cassette tape.',
-          choice2: 'It\'s him or me! You take the knife and stab him.'),
-      Story(
-          storyTitle:
-              'What? Such a cop out! Did you know traffic accidents are the second leading cause of accidental death for most adult age groups?',
-          choice1: 'Restart',
-          choice2: ''),
-      Story(
-          storyTitle:
-              'As you smash through the guardrail and careen towards the jagged rocks below you reflect on the dubious wisdom of stabbing someone while they are driving a car you are in.',
-          choice1: 'Restart',
-          choice2: ''),
-      Story(
-          storyTitle:
-              'You bond with the murderer while crooning verses of "Can you feel the love tonight". He drops you off at the next town. Before you go he asks you if you know any good places to dump bodies. You reply: "Try the pier".',
-          choice1: 'Restart',
-          choice2: '')
-    ];
+    if (path == null) {
+      _currentStory = null;
+      return;
+    }
+
+    switch (choice) {
+      case StoryChoice.first:
+        {
+          _currentStory = path.firstChoice;
+          break;
+        }
+
+      case StoryChoice.second:
+        {
+          _currentStory = path.secondChoice;
+          break;
+        }
+    }
   }
 }
 
 //TODO: Step 23 - Use the storyNumber property inside getStory(), getChoice1() and getChoice2() so that it gets the updated story and choices rather than always just the first (0th) one.
 
 //TODO: Step 25 - Change the storyNumber property into a private property so that only story_brain.dart has access to it. You can do this by right clicking on the name (storyNumber) and selecting Refactor -> Rename to make the change across all the places where it's used.
-
-//TODO: Step 20 - Download the story plan here: https://drive.google.com/uc?export=download&id=1KU6EghkO9Hf2hRM0756xFHgNaZyGCou3
 
 //TODO: Step 21 - Using the story plan, update nextStory() to change the storyNumber depending on the choice made by the user. e.g. if choiceNumber was equal to 1 and the storyNumber is 0, the storyNumber should become 2.
 
