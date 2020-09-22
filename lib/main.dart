d70a06c870618d548146e24f7b390f08d7eca22a
Thomas M <twsm000@gmail.com>
2020-09-22 18:17:37 -0300

    Add visibility to button

--- lib/main.dart
+++ lib/main.dart
@@ -1,8 +1,6 @@
 import 'package:destini_challenge_starting/story_brain.dart';
 import 'package:flutter/material.dart';
 
-//TODO: Step 15 - Run the app and see if you can see the screen update with the first story. Delete this TODO if it looks as you expected.
-
 void main() => runApp(Destini());
 
 class Destini extends StatelessWidget {
@@ -68,8 +66,9 @@ class _StoryPageState extends State<StoryPage> {
         flex: 2,
         child: FlatButton(
           onPressed: () {
-            //Choice 1 made by user.
-            _stories.nextStory(StoryChoice.first);
+            setState(() {
+              _stories.nextStory(StoryChoice.first);
+            });
           },
           color: Colors.red,
           child: Text(
@@ -85,18 +84,20 @@ class _StoryPageState extends State<StoryPage> {
       ),
       Expanded(
         flex: 2,
-        //TODO: Step 26 - Use a Flutter Visibility Widget to wrap this FlatButton.
-        //TODO: Step 28 - Set the "visible" property of the Visibility Widget to equal the output from the buttonShouldBeVisible() method in the storyBrain.
-        child: FlatButton(
-          onPressed: () {
-            //Choice 2 made by user.
-            _stories.nextStory(StoryChoice.second);
-          },
-          color: Colors.blue,
-          child: Text(
-            _stories.getSecondChoice(),
-            style: TextStyle(
-              fontSize: 20.0,
+        child: Visibility(
+          visible: _stories.buttonShouldBeVisible(),
+          child: FlatButton(
+            onPressed: () {
+              setState(() {
+                _stories.nextStory(StoryChoice.second);
+              });
+            },
+            color: Colors.blue,
+            child: Text(
+              _stories.getSecondChoice(),
+              style: TextStyle(
+                fontSize: 20.0,
+              ),
             ),
           ),
         ),
@@ -104,7 +105,5 @@ class _StoryPageState extends State<StoryPage> {
     ];
   }
 }
-
-//TODO: Step 24 - Run the app and try to figure out what code you need to add to this file to make the story change when you press on the choice buttons.
 
 //TODO: Step 29 - Run the app and test it against the Story Outline to make sure you've completed all the steps. The code for the completed app can be found here: https://github.com/londonappbrewery/destini-challenge-completed/
