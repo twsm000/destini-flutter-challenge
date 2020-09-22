998628b2f33b69358c4ffa27dd71bec5cd006a79
Thomas M <twsm000@gmail.com>
2020-09-22 14:56:02 -0300

    Add internal methods to iterate over the stories
    - Add Constructor initialization.
    - Add hasNext().
    - Add getStory().

--- lib/main.dart
+++ lib/main.dart
@@ -1,3 +1,4 @@
+import 'package:destini_challenge_starting/story_brain.dart';
 import 'package:flutter/material.dart';
 
 //TODO: Step 15 - Run the app and see if you can see the screen update with the first story. Delete this TODO if it looks as you expected.
@@ -13,23 +14,22 @@ class Destini extends StatelessWidget {
   }
 }
 
-//TODO: Step 9 - Create a new storyBrain object from the StoryBrain class.
-
 class StoryPage extends StatefulWidget {
   _StoryPageState createState() => _StoryPageState();
 }
 
 class _StoryPageState extends State<StoryPage> {
+  StoryBrain _stories = StoryBrain();
+
   @override
   Widget build(BuildContext context) {
+    return _getMainPage();
+  }
+
+  Widget _getMainPage() {
     return Scaffold(
       body: Container(
-        decoration: BoxDecoration(
-          image: DecorationImage(
-            image: AssetImage("images/background.png"),
-            fit: BoxFit.fill,
-          ),
-        ),
+        decoration: _getBackground(),
         padding: EdgeInsets.symmetric(vertical: 50.0, horizontal: 15.0),
         constraints: BoxConstraints.expand(),
         child: SafeArea(
@@ -40,8 +40,7 @@ class _StoryPageState extends State<StoryPage> {
                 flex: 12,
                 child: Center(
                   child: Text(
-                    //TODO: Step 10 - use the storyBrain to get the first story title and display it in this Text Widget.
-                    'Story text will go here.',
+                    _stories.getStory(),
                     style: TextStyle(
                       fontSize: 25.0,
                     ),
@@ -90,6 +89,15 @@ class _StoryPageState extends State<StoryPage> {
             ],
           ),
         ),
+      ),
+    );
+  }
+
+  Decoration _getBackground() {
+    return BoxDecoration(
+      image: DecorationImage(
+        image: AssetImage("images/background.png"),
+        fit: BoxFit.fill,
       ),
     );
   }
